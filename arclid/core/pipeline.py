fc28cdfbbc850515580d7d97733dc913f7ccd4fc
Sajad <41271921+sajadtavakoli@users.noreply.github.com>
2025-10-07 16:24:06 +0200

    Create pipeline.py

---
+++ arclid/core/pipeline.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from .model_loader import load_model
+from ..io.bam_reader import open_bam
+from ..io.vcf_writer import VCFWriter
+
+
+
+def run_arclid(aln_path, ref_path, out_path, cov, threads, sample, contigs, fast):
+  pass 
