5a26e8b1a3a4afe01a06dab0aa3852a4228dc80f
Sajad Tavakoli <41271921+sajadtavakoli@users.noreply.github.com>
2025-10-13 02:37:27 +0200

    model_loader.py for hifi models

--- arclid/core/model_loader.py
+++ arclid/core/model_loader.py
@@ -1,12 +1,31 @@
 from ultralytics import YOLO
+from pathlib import Path
+import urllib.request
+
+HIFI_5K_URL = "https://drive.google.com/file/d/17jDHlwMyQHtjE269QVe5yaGd9tkY58cX/view?usp=sharing"
+
+HIFI_50k_URL = "https://drive.google.com/file/d/1nKgiPE3rD7ag6YULb1OO-MN-7cF4Qlpj/view?usp=sharing"
 
 
-def load_model():
-    # out_path = args.out_path
-    model_path_5k = '/zhome/55/8/198316/projects/variant-calling/run19_cutsize5k_resize1280_2models4HIFIandONT/ai_model_hifi/runs/detect/yolo11x_ccs_filtered_100perc_empty/weights/best.pt'
-    model_path_50k = '/zhome/55/8/198316/projects/variant-calling/run20_cutesize50k/ai_model_hifi/runs/detect/yolo11x_ccs_filtered_100perc_empty/weights/best.pt'
+def load_model(model_path_hifi_5k: str | None = None, model_path_hifi_50k: str | None = None):
+    if model_path_hifi_5k is None:
+        model_path_hifi_5k = Path(__file__).resolve().parent.parent / "weights" / "model_hifi_5k.pt"
+    model_path_hifi_5k = Path(model_path_hifi_5k)
 
-    model_5k = YOLO(model_path_5k)
-    model_50k = YOLO(model_path_50k)
-    
-    return model_5k, model_50k
+    if model_path_hifi_50k is None:
+        model_path_hifi_50k = Path(__file__).resolve().parent.parent / "weights" / "model_hifi_50k.pt"
+    model_path_hifi_50k = Path(model_path_hifi_50k)
+
+    if not model_path_hifi_5k.exists():
+        model_path_hifi_5k.parent.mkdir(parents=True, exist_ok=True)
+        print(f"[ARCLID] Downloading hifi model 1 ...")
+        urllib.request.urlretrieve(HIFI_5K_URL, model_path_hifi_5k)
+    model_hifi_5k = YOLO(str(model_path_hifi_5k))
+
+    if not model_path_hifi_50k.exists():
+        model_path_hifi_50k.parent.mkdir(parents=True, exist_ok=True)
+        print(f"[ARCLID] Downloading hifi model 2 ...")
+        urllib.request.urlretrieve(HIFI_50k_URL, model_path_hifi_50k)
+    model_hifi_50k = YOLO(str(model_path_hifi_50k))
+
+    return model_hifi_5k, model_hifi_50k
