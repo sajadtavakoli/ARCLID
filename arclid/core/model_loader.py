f906f1f842a969e74e3004a3339413e3010e73bc
Sajad <41271921+sajadtavakoli@users.noreply.github.com>
2026-03-11 23:06:37 +0100

    Update model URLs in model_loader.py

--- arclid/core/model_loader.py
+++ arclid/core/model_loader.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 from pathlib import Path
 import urllib.request
 
-HIFI_5K_URL = "https://drive.google.com/file/d/17jDHlwMyQHtjE269QVe5yaGd9tkY58cX/view?usp=sharing"
 
-HIFI_50k_URL = "https://drive.google.com/file/d/1nKgiPE3rD7ag6YULb1OO-MN-7cF4Qlpj/view?usp=sharing"
+HIFI_5K_URL = "https://drive.usercontent.google.com/download?id=17jDHlwMyQHtjE269QVe5yaGd9tkY58cX&export=download&authuser=0&confirm=t&uuid=7733230b-b097-40ba-93cf-3cdd54dee221&at=AGN2oQ2mPF99EDpCtjV3YHwl4UDR%3A1773260064723"
 
+HIFI_50K_URL = "https://drive.usercontent.google.com/download?id=1nKgiPE3rD7ag6YULb1OO-MN-7cF4Qlpj&export=download&authuser=0&confirm=t&uuid=b237b464-fc81-4324-9bd4-3952aae83515&at=AGN2oQ3SO-K09rtFb12QFTMn5XXU%3A1773260279916"
 
 def load_model(model_path_hifi_5k: str | None = None, model_path_hifi_50k: str | None = None):
     if model_path_hifi_5k is None:
@@ -25,7 +25,7 @@ def load_model(model_path_hifi_5k: str | None = None, model_path_hifi_50k: str |
     if not model_path_hifi_50k.exists():
         model_path_hifi_50k.parent.mkdir(parents=True, exist_ok=True)
         print(f"[ARCLID] Downloading hifi model 2 ...")
-        urllib.request.urlretrieve(HIFI_50k_URL, model_path_hifi_50k)
+        urllib.request.urlretrieve(HIFI_50K_URL, model_path_hifi_50k)
     model_hifi_50k = YOLO(str(model_path_hifi_50k))
 
     return model_hifi_5k, model_hifi_50k
